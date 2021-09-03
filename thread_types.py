4fcfac8bfa91d9108503c2f35ba87b7ff65392d3
Benjamin Hourte <benjamin.hourte@earthlab.lu>
2021-09-03 10:09:34 +0200

    Update of the thread types to match DTM ones

--- thread_types.py
+++ thread_types.py
@@ -1,1 +1,16 @@
-THREAD_TYPES = ["DDOS", "MALWARE", "SCAN"]
+THREAD_TYPES = [
+    "TCP Null Flood",
+    "SYN Flood",
+    "ACK Flood",
+    "RST Flood",
+    "SYN-ACK Flood",
+    "Christmas Tree Packet Attack(FPU)",
+    "Christmas Tree Packet Attack(0X40)",
+    "Christmas Tree Packet Attack(0X40)",
+    "UDP Flood",
+    "ICMP Flood",
+    "HTTP GET Flood",
+    "HTTP POST Flood",
+    "Slowloris",
+    "DNS amplification",
+]
